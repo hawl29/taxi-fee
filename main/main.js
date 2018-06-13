392e17488d5c70b7412ae914b8c1df2b4b24ae89
hawl29 <cz_hawl29@163.com>
2018-06-13 15:56:31 +0800

    complete

--- main/main.js
+++ main/main.js
@@ -1,4 +1,10 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function main(mileage,wait_time)
+{
+    let mil_price,total_price;
+    if(mileage<=2) mil_price=6;
+    else if(mileage<=8) mil_price=6+(mileage-2)*0.8;
+    else mil_price=10.8+(mileage-8)*1.2;
+    total_price=mil_price+wait_time*0.25;
+    return Math.round(total_price);
 };
+    
--- spec/main-spec.js
+++ spec/main-spec.js
@@ -2,4 +2,27 @@ const main = require('../main/main');
 
 describe('taxi fee', function () {
     // write your tests here...
+    it('里程2公里以内，无等待', function()
+    {
+       var result = main(1.6,0);
+       expect(result).toEqual(6);
+    })
+
+    it('里程大于2公里小于8公里，无等待', function()
+    {
+        var result = main(5.6,0);
+        expect(result).toEqual(9);
+    })
+
+    it('里程大于8公里，无等待', function()
+    {
+        var result = main(16.2,0);
+        expect(result).toEqual(21);
+    })
+
+    it('里程8公里，有等待', function()
+    {
+        var result = main(16.2,25);
+        expect(result).toEqual(27);
+    })
 });
